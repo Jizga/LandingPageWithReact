601606177b09201d3f0b587cbcda14b7109b29a5
Jizga <jizga93@gmail.com>
2021-06-08 20:18:05 +0000

    Album component added to improve the code

---
+++ src/js/component/album.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Card } from "./card";
+
+export function Album() {
+	return (
+		<div>
+			<div className="album py-5">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-3">
+							<Card
+								title="My first imagen"
+								description="This imagine is about a cat with a dog"
+								img="https://www.zooplus.es/magazine/wp-content/uploads/2019/10/featured-img-2-768x576.jpg"
+							/>
+						</div>
+
+						<div className="col-md-3">
+							<Card
+								title="My second imagen"
+								description="This imagine is about a tattoo cat"
+								img="https://lh3.googleusercontent.com/proxy/MPRjgiebJ9Z1Dv2NlnyBylpd0-HhXC8COxsNF0xSiCnso7fefH4U3g2h3jYgaq_p9byik3SOtGyOTmSDhyYQ2UhVibn7rDXKm-4e4pG0Lavns5jV4nPoLBNU52eodfKGC33y8OrfQC0WYVTAK6tMsdH1QdAHtXkap7_EGKqCYKwX"
+							/>
+						</div>
+
+						<div className="col-md-3">
+							<Card
+								title="My third imagen"
+								description="This imagine is about a girl with her cat"
+								img="https://i.pinimg.com/564x/32/3c/92/323c9276b2c0dd7d2c3a407c6250f880.jpg"
+							/>
+						</div>
+
+						<div className="col-md-3">
+							<Card
+								title="My fourth imagen"
+								description="This imagine is about a boy with his dog"
+								img="https://i.blogs.es/cd8e21/istock_38997540_medium/1366_2000.jpg"
+							/>
+						</div>
+
+						<div className="col-md-3">
+							<Card
+								title="My fifth imagen"
+								description="This imagine is about a boy with his dog"
+								img="https://i.blogs.es/cd8e21/istock_38997540_medium/1366_2000.jpg"
+							/>
+						</div>
+
+						<div className="col-md-3">
+							<Card
+								title="My sixth imagen"
+								description="This imagine is about a boy with his dog"
+								img="https://i.blogs.es/cd8e21/istock_38997540_medium/1366_2000.jpg"
+							/>
+						</div>
+
+						<div className="col-md-3">
+							<Card
+								title="My seventh imagen"
+								description="This imagine is about a boy with his dog"
+								img="https://i.blogs.es/cd8e21/istock_38997540_medium/1366_2000.jpg"
+							/>
+						</div>
+
+						<div className="col-md-3">
+							<Card
+								title="My eighth imagen"
+								description="This imagine is about a boy with his dog"
+								img="https://i.blogs.es/cd8e21/istock_38997540_medium/1366_2000.jpg"
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar } from "./navbar";
 import { Jumbotron } from "./jumbotron";
-import { Card } from "./card";
+import { Album } from "./album";
 
 //create your first component
 export function Home() {
@@ -9,21 +9,7 @@ export function Home() {
 		<div>
 			<Navbar />
 			<Jumbotron />
-			<div>
-				<div className="album py-5">
-					<div className="container">
-						<div className="row">
-							<div className="col-md-4">
-								<Card
-									title="My first imagen"
-									description="This imagen is about a cat with a dog"
-									img="https://www.zooplus.es/magazine/wp-content/uploads/2019/10/featured-img-2-768x576.jpg"
-								/>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			<Album />
 		</div>
 	);
 }
