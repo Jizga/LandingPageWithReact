034df03097c0a07784ef5dc9b55dee360f7ddd30
Jizga <jizga93@gmail.com>
2021-06-09 19:25:37 +0000

    Updated album component with a muckear data

---
+++ src/js/data.js
@@ -0,0 +1,55 @@
+export const data = [
+	{
+		id: 1,
+		title: "My first imagen",
+		description: "This imagine is about a cat with a dog",
+		img:
+			"https://www.zooplus.es/magazine/wp-content/uploads/2019/10/featured-img-2-768x576.jpg"
+	},
+	{
+		id: 2,
+		title: "My second imagen",
+		description: "This imagine is about a tattoo cat",
+		img:
+			"https://lh3.googleusercontent.com/proxy/MPRjgiebJ9Z1Dv2NlnyBylpd0-HhXC8COxsNF0xSiCnso7fefH4U3g2h3jYgaq_p9byik3SOtGyOTmSDhyYQ2UhVibn7rDXKm-4e4pG0Lavns5jV4nPoLBNU52eodfKGC33y8OrfQC0WYVTAK6tMsdH1QdAHtXkap7_EGKqCYKwX"
+	},
+	{
+		id: 3,
+		title: "My third imagen",
+		description: "This imagine is about a girl with her cat",
+		img:
+			"https://i.pinimg.com/564x/32/3c/92/323c9276b2c0dd7d2c3a407c6250f880.jpg"
+	},
+	{
+		id: 4,
+		title: "My fourth imagen",
+		description: "This imagine is about a boy with his dog",
+		img: "https://i.blogs.es/cd8e21/istock_38997540_medium/1366_2000.jpg"
+	},
+	{
+		id: 5,
+		title: "My fifth imagen",
+		description: "This imagine is about a boy dog",
+		img: "https://i.blogs.es/d099a0/perro/840_560.jpg"
+	},
+	{
+		id: 6,
+		title: "My sixth imagen",
+		description: "This imagine is about a dog in the countryside",
+		img:
+			"https://verdecora.es/blog/wp-content/uploads/2019/09/como-saber-perro-es-feliz.jpg"
+	},
+	{
+		id: 7,
+		title: "My seventh imagen",
+		description: "This imagine is about 4 friends",
+		img: "https://aws.traveler.es/prod/designs/v1/assets/940x627/91190.jpg"
+	},
+	{
+		id: 8,
+		title: "My eighth imagen",
+		description: "This imagine is about a women in a train",
+		img:
+			"https://static.vix.com/es/sites/default/files/styles/1x1/public/v/viajar-en-tren-ventajas-mexico.jpg"
+	}
+];
